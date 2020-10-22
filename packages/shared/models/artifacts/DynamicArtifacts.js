b5b498e0b45c704e325578fa35fff79e49fe5c91
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-10-22 16:35:57 -0300

    shared: fix dynamic artifacts

--- packages/shared/models/artifacts/DynamicArtifacts.js
+++ packages/shared/models/artifacts/DynamicArtifacts.js
@@ -1,21 +1,31 @@
+const fs = require('fs')
 const path = require('path')
 const BaseArtifacts = require('./BaseArtifacts')
 
+const BUILD_DIRS = ['build/contracts', 'artifacts']
+
 class DynamicArtifacts extends BaseArtifacts {
   getContractSchema(contractName, dependency = undefined) {
-    const contractPath = dependency
+    const contractPaths = dependency
       ? this._getNodeModulesPath(dependency, contractName)
       : this._getLocalBuildPath(contractName)
 
-    return require(contractPath)
+    const artifact = this._findArtifact(contractPaths)
+    if (!artifact) throw Error(`Could not find artifact for ${dependency} ${contractName}`)
+    return artifact
+  }
+
+  _findArtifact(paths) {
+    const path = paths.find(fs.existsSync)
+    return path ? require(path) : undefined
   }
 
   _getLocalBuildPath(contractName) {
-    return path.resolve(process.cwd(), `./build/contracts/${contractName}.json`)
+    return BUILD_DIRS.map(dir => path.resolve(process.cwd(), `./${dir}/${contractName}.json`))
   }
 
   _getNodeModulesPath(dependency, contractName) {
-    return path.resolve(__dirname, `../../node_modules/${dependency}/build/contracts/${contractName}.json`)
+    return BUILD_DIRS.map(dir => path.resolve(__dirname, `../../node_modules/${dependency}/${dir}/${contractName}.json`))
   }
 }
 

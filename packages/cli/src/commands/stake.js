54c85f4f7aeaf2b13de2ab8d3537bea682803ab8
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-10-21 21:58:33 -0300

    cli: avoid anj usage

--- packages/cli/src/commands/stake.js
+++ packages/cli/src/commands/stake.js
@@ -1,18 +1,18 @@
 const logger = require('@aragon/protocol-backend-shared/helpers/logger')('stake')
 
 const command = 'stake'
-const describe = 'Stake ANJ tokens for a guardian'
+const describe = 'Stake tokens for a guardian'
 
 const builder = {
   guardian: { alias: 'j', describe: 'Address of the guardian staking the tokens', type: 'string', demand: true },
-  amount: { alias: 'a', describe: 'Number of ANJ tokens to stake', type: 'string', demand: true },
+  amount: { alias: 'a', describe: 'Number of tokens to stake', type: 'string', demand: true },
   data: { alias: 'd', describe: 'Optional data that can be used to ask for token activation', type: 'string' },
 }
 
 const handlerAsync = async (environment, { guardian, amount, data }) => {
   const court = await environment.getCourt()
   await court.stake(guardian, amount, data)
-  logger.success(`Staked ${amount} ANJ for ${guardian}`)
+  logger.success(`Staked ${amount} for ${guardian}`)
 }
 
 module.exports = {

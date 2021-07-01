1b8a797260d3cf5abf8cf6936cb41c770aed65fd
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-07-01 19:50:50 +0400

    changed truffle config

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -9,9 +9,9 @@ config.networks.staging = Object.assign({}, { ...config.networks.rinkeby })
 const { networks: { ganache, ropsten, rinkeby, staging, mainnet } } = config
 
 ganache.court = undefined
-staging.court = '0x2057Fa53c5c85bB2cff125f9DB2D0cA7E4eeBE02'
+staging.court = '0xD2c15eCd1751C2cE8b02ab2D95db32E662517D61'
 ropsten.court = '0x7639480251C12f8168eeEc5e815Ab96072E5fe62'
-rinkeby.court = '0xDB56c4d44ba23133805A38d837aBeC811D6c28b9'
-mainnet.court = '0xee4650cBe7a2B23701D416f58b41D8B76b617797'
+rinkeby.court = '0xC464EB732A1D2f5BbD705727576065C91B2E9f18'
+mainnet.court = '0xFb072baA713B01cE944A0515c3e1e98170977dAF'
 
 module.exports = config

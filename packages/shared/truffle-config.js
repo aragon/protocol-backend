203a49fc049a53f56dce3649e55b808ce957616f
Giorgi Lagidze <glagh14@freeuni.edu.ge>
2021-07-26 14:04:25 +0400

    change staging court address

--- packages/shared/truffle-config.js
+++ packages/shared/truffle-config.js
@@ -9,7 +9,7 @@ config.networks.staging = Object.assign({}, { ...config.networks.rinkeby })
 const { networks: { ganache, ropsten, rinkeby, staging, mainnet } } = config
 
 ganache.court = undefined
-staging.court = '0xD2c15eCd1751C2cE8b02ab2D95db32E662517D61'
+staging.court = '0x9c003ec97676c30a041f128d671b3db2f790c3e7'
 ropsten.court = '0x7639480251C12f8168eeEc5e815Ab96072E5fe62'
 rinkeby.court = '0xC464EB732A1D2f5BbD705727576065C91B2E9f18'
 mainnet.court = '0xFb072baA713B01cE944A0515c3e1e98170977dAF'

c6091b8fb013c08839be94d6ea7c6a895ae4ff8b
Mathias Scherer <mathias@aragon.org>
2021-07-27 12:04:24 +0200

    rebuild ci/cd pipeline and docker image

--- .github/scripts/emails-sync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export POSTMARK_SERVER_API_TOKEN="$1"
-cd emails
-yarn install
-yarn sync:assets
-yarn sync:templates
--- .github/scripts/kubectl-set-image.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-DEPLOYMENT="$1"
-IMAGE="$2"
-# replace deployment image
-kubectl set image deployment/$DEPLOYMENT server=$IMAGE services=$IMAGE app=$IMAGE
